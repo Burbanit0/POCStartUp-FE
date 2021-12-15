3976ddcbabc676a48f0e144e51840902ca8acd2e
sgourraud <72745033+sgourraud@users.noreply.github.com>
2021-12-15 15:43:30 +0100

    Add files via upload

--- angularclient/src/app/app-routing.module.ts
+++ angularclient/src/app/app-routing.module.ts
@@ -5,11 +5,23 @@ import { UserFormComponent } from './user-form/user-form.component';
 import { ProjectFormComponent } from './project-form/project-form.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { BoardUserComponent } from './board-user/board-user.component';
+import { BoardAdminComponent } from './board-admin/board-admin.component';
+
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'adduser', component: UserFormComponent },
   { path: 'projects', component: ProjectListComponent},
-  { path: 'addproject', component: ProjectFormComponent}
+  { path: 'addproject', component: ProjectFormComponent},
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'user', component: BoardUserComponent },
+  { path: 'admin', component: BoardAdminComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
