5ec278d7bf0e51140ae0ddbfc58632a169ccc762
Matthieu Burban <matthieuburban@wifiroam059188.univ-st-etienne.fr>
2022-02-02 16:12:49 +0100

    visuel de la liste des projets

--- angularclient/src/app/board-user/board-user.component.ts
+++ angularclient/src/app/board-user/board-user.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import { ProjectService } from '../services/project.service';
 import { TokenStorageService } from '../_services/token-storage.service';
 import { User } from '../models/user.model';
+import { Project } from '../models/project.model';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -12,6 +13,9 @@ import { UserService } from '../services/user.service';
 export class BoardUserComponent implements OnInit {
   currentUser: any;
   currentUserId: any;
+  projects?: Project[];
+  currentProject: Project = {};
+  currentIndex = -1;
   user: User = {
     name: '',
     roles: [{id:'', name:''}],
@@ -30,10 +34,16 @@ export class BoardUserComponent implements OnInit {
     this.userService.get(this.currentUserId).subscribe(
       data => {
         this.user = data;
+        this.projects = this.user.projects;
         // console.log(data);
       },
       error => {
         console.log(error);
       });
+  }
+
+  setActiveProject(project: Project, index: number): void {
+    this.currentProject = project;
+    this.currentIndex = index;
   }
 }
