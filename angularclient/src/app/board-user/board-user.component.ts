5941de24cfef724127b3f3a1325dba3bd77e3cbe
Matthieu Burban <matthieuburban@MacBook-Pro-de-Matthieu.local>
2022-02-03 17:04:01 +0100

    worktime modal

--- angularclient/src/app/board-user/board-user.component.ts
+++ angularclient/src/app/board-user/board-user.component.ts
@@ -4,6 +4,8 @@ import { TokenStorageService } from '../_services/token-storage.service';
 import { User } from '../models/user.model';
 import { Project } from '../models/project.model';
 import { UserService } from '../services/user.service';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-board-user',
@@ -25,8 +27,28 @@ export class BoardUserComponent implements OnInit {
 
   constructor(
     private token: TokenStorageService,
-    private userService: UserService) { }
+    private userService: UserService,
+    private modalService: NgbModal) { }
 
+    closeResult = '';
+  
+    open(content:any) {
+      this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+  
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return `with: ${reason}`;
+      }
+    }
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
     this.currentUserId = this.currentUser.id;
